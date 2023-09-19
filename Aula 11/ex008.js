5e8827c8ac4aad65b34c291ab8d98eec10602d05
IMadsoon <phsraviila@gmail.com>
2023-09-19 10:52:30 -0300

    versao teste

---
+++ Aula 11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 45
+console.log(`a velocidade do seu carro é ${vel}km/h `)
+if(vel > 60){ //condição simples !
+    console.log('Voce ultrapassou a velocidade permitida. MULTADO !')
+} else{
+    console.log('Passou livre')
+}
+console.log('dirija sempre usando cinto de segurança')
